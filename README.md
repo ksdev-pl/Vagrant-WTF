d380ecfa12ac9aa2db295e15e6b72463c26948ea
KS <ksdev-pl@users.noreply.github.com>
2014-04-07 07:03:40 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Vagrant-WTF
-===========
+![Vagrant-WTF](https://dl.dropboxusercontent.com/u/143578/temp/vagrant_wtf.png)
+
+You should probably use one of the other options for Vagrant configuration, like:
+* [__PuPHPet__](https://puphpet.com/)
+* [__Protobox__](http://getprotobox.com/)
+* [__Jeffrey Way tutorial__](https://gist.github.com/JeffreyWay/af0ee7311abfde3e3b73)
+* [__Vaprobash__](http://fideloper.github.io/Vaprobash/index.html)
+
+This repo is just bare bones LAMP configuration for beginners, easy to understand & modify.
---
+++ vagrant.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server php5-mysql php5-xdebug
+
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9000
+xdebug.scream = 1
+xdebug.cli_color = 1
+xdebug.show_local_vars = 1
+EOF
+
+sudo a2enmod rewrite
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sed -i "s/html_errors = .*/html_errors = On/" /etc/php5/apache2/php.ini
+
+sudo service apache2 restart
+
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+MYSQL=`which mysql`
+Q1="GRANT ALL ON *.* TO 'root'@'192.168.33.1' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+Q2="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}"
+$MYSQL -uroot -proot -e "$SQL"
+
+sudo service mysql restart
